5386f945a5e82b4909f74bd7d0b0473aaa00c4ab
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2024-12-14 16:41:38 -0500

    fix: font size

--- src/pages/Counter/Counter.tsx
+++ src/pages/Counter/Counter.tsx
@@ -23,27 +23,27 @@ const ShowCounter: React.FC = () => {
 
 
     <div className="flex flex-col justify-center items-center h-screen">
-      <Card className="w-1/2">
+      <Card className="w-1/2 rounded-2xl border-2 shadow">
         <CardHeader >
           <CardTitle className="text-center text-6xl">Donaciones</CardTitle>
         </CardHeader>
         <CardContent>
-          <h1 className="text-green-500 text-center my-12 text-5xl font-bold">${total.toLocaleString()}</h1>
+          <h1 className="text-green-500 text-center my-12 text-6xl font-bold">${total.toLocaleString()}</h1>
           {donations.length === 0 ? (
             <p className="text-gray-600">No hay donaciones aún.</p>
           ) : (
             <Table className="w-96 mx-auto">
               <TableHeader>
-                <TableRow>
-                  <TableHead className="text-left">Cantidad</TableHead>
-                  <TableHead className="text-center">Fecha</TableHead>
+                <TableRow >
+                  <TableHead className="text-left w-1/2">Cantidad</TableHead>
+                  <TableHead className="text-left w-1/2">Fecha</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
               {donations.map((donation) => (
-                <TableRow>
-                  <TableCell key={donation.id} className="font-medium text-left">${donation.amount.toLocaleString()}</TableCell>
-                  <TableCell className="text-right">{new Date(donation.timestamp).toLocaleString()}</TableCell>
+                <TableRow key={donation.id}>
+                  <TableCell  className="font-medium text-left">${donation.amount.toLocaleString()}</TableCell>
+                  <TableCell className="text-left">{new Date(donation.timestamp).toLocaleString()}</TableCell>
                 </TableRow>
               ))}
               </TableBody>
